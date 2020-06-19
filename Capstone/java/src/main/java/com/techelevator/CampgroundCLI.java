1dbe0fb6899e0a5f3b65a728acbbdbc70efae9cf
kristopher bartow <krbartow@gmail.com>
2020-06-18 21:28:27 -0400

    finishing menu

--- Capstone/java/src/main/java/com/techelevator/CampgroundCLI.java
+++ Capstone/java/src/main/java/com/techelevator/CampgroundCLI.java
@@ -1,11 +1,32 @@
 package com.techelevator;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import com.techelevator.model.Park;
+import com.techelevator.model.jdbc.JDBCCampgroundDAO;
+import com.techelevator.model.jdbc.JDBCParkDAO;
+import com.techelevator.model.jdbc.JDBCReservationDAO;
+import com.techelevator.model.jdbc.JDBCSiteDAO;
+import com.techelevator.view.MainMenu;
+import com.techelevator.view.Menu;
+
 public class CampgroundCLI {
 
+	private static final String MENU_OPTION_VIEW_CAMPGROUNDS = "View Campgrounds";
+	private static final String MENU_OPTION_SEARCH_FOR_RESERVATON = "Search for Reservation";
+	private static final String MENU_OPTION_RETURN = "Return to Previous Screen";
+	private static final String[] PARK_MENU_OPTIONS = {MENU_OPTION_VIEW_CAMPGROUNDS,  MENU_OPTION_SEARCH_FOR_RESERVATON, MENU_OPTION_RETURN};
+	
+	
+	private List<Park> parkList;
+	private Park chosenPark;
+	private Menu menu;
+	
+	
 	public static void main(String[] args) {
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setUrl("jdbc:postgresql://localhost:5432/campground");
@@ -17,10 +38,56 @@ public class CampgroundCLI {
 	}
 
 	public CampgroundCLI(DataSource datasource) {
-		// create your DAOs here
+		JDBCParkDAO pDAO = new JDBCParkDAO(datasource);
+		JDBCSiteDAO sDAO = new JDBCSiteDAO(datasource);
+		JDBCReservationDAO rDAO = new JDBCReservationDAO(datasource);
+		JDBCCampgroundDAO cDAO = new JDBCCampgroundDAO(datasource);
+		parkList = pDAO.getAllParks();
+		menu = new Menu(System.in, System.out); 
 	}
 
 	public void run() {
 
+		MainMenu mainMenu = new MainMenu(System.in, System.out);
+
+		String[] parkNames = new String[parkList.size()];
+
+		for (int i = 0; i < parkNames.length; i++) {
+			parkNames[i] = parkList.get(i).getName();
+		}
+		while (true) {
+			String choice = (String) mainMenu.getChoiceFromOptions(parkNames);
+			
+			if (choice.equals("quit")) {
+				System.exit(1);
+
+			} else {
+				for (Park p : parkList) {
+					if (choice.equals(p.getName())) {
+						chosenPark = p;
+					}
+				}
+				chosenPark.displayInfo();
+				parkMenu(chosenPark.getParkId());
+			}
+		}
+
 	}
+	
+	private void parkMenu( long parkId) {
+		while (true) {
+			String choice = (String)menu.getChoiceFromOptions(PARK_MENU_OPTIONS);
+			if ( choice.equals(MENU_OPTION_VIEW_CAMPGROUNDS)) {
+				//campMenu(parkId); 	
+			}else if(choice.equals(MENU_OPTION_SEARCH_FOR_RESERVATON)){ 
+			//	searchParkWide();
+			} else if( choice.equals(MENU_OPTION_RETURN)) {
+				break;
+			}
+			
+		}
+		
+	}
+	
+	
 }
---
+++ Capstone/java/src/main/java/com/techelevator/view/MainMenu.java
@@ -0,0 +1,46 @@
+package com.techelevator.view;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainMenu extends Menu {
+	public MainMenu(InputStream input, OutputStream output) {
+		super(input, output);
+	}
+
+	@Override
+	protected Object getChoiceFromUserInput(Object[] options) {
+		Object choice = null;
+		String userInput = in.nextLine();
+		try {
+			if (userInput.equalsIgnoreCase("q")) {
+				choice = "quit";
+			} else {
+				int selectedOption = Integer.valueOf(userInput);
+				if (selectedOption > 0 && selectedOption <= options.length) {
+					choice = options[selectedOption - 1];
+				}
+			}
+		} catch (NumberFormatException e) {
+			// eat the exception, an error message will be displayed below since choice will
+			// be null
+		}
+		if (choice == null) {
+			out.println("\n*** " + userInput + " is not a valid option ***\n");
+		}
+		return choice;
+	}
+
+	@Override
+	protected void displayMenuOptions(Object[] options) {
+		out.println();
+		for (int i = 0; i < options.length; i++) {
+			int optionNum = i + 1;
+			out.println(optionNum + ") " + options[i]);
+		}
+		
+		out.println( "Q) quit");
+		out.print("\nPlease choose an option >>> ");
+		out.flush();
+	}
+}
